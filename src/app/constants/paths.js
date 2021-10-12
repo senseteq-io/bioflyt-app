1511de9423b5450dab42a4dc6646cbb80fd27b50
Maxim Makarov <maxim.makarov14@gmail.com>
2021-10-12 18:34:34 +0300

    add paths for therapist

--- src/app/constants/paths.js
+++ src/app/constants/paths.js
@@ -1,33 +1,58 @@
-export const BIOFLYT_PATH = '/bioflow/admin'
+export const BIOFLYT_ADMIN_PATH = '/bioflow/admin'
 
-export const BIOFLYT_GROUPS_PATH = '/bioflow/admin/groups'
-export const BIOFLYT_GROUP_SHOW_PATH = '/bioflow/admin/groups/:id'
-export const BIOFLYT_GROUP_EDIT_PATH = '/bioflow/admin/groups/:id/edit'
-export const BIOFLYT_GROUP_CREATE_PATH = '/bioflow/admin/group/create'
-export const BIOFLYT_GROUP_ACTIVITIES_PATH =
+export const BIOFLYT_ADMIN_GROUPS_PATH = '/bioflow/admin/groups'
+export const BIOFLYT_ADMIN_GROUP_SHOW_PATH = '/bioflow/admin/groups/:id'
+export const BIOFLYT_ADMIN_GROUP_EDIT_PATH = '/bioflow/admin/groups/:id/edit'
+export const BIOFLYT_ADMIN_GROUP_CREATE_PATH = '/bioflow/admin/group/create'
+export const BIOFLYT_ADMIN_GROUP_ACTIVITIES_PATH =
   '/bioflow/admin/groups/:id/activities'
-export const BIOFLYT_GROUP_PATIENTS_PATH = '/bioflow/admin/groups/:id/patients'
+export const BIOFLYT_ADMIN_GROUP_PATIENTS_PATH =
+  '/bioflow/admin/groups/:id/patients'
 
-export const BIOFLYT_GLOBAL_ACTIVITIES_PATH = '/bioflow/admin/activities'
-export const BIOFLYT_CLINICS_PATH = '/bioflow/admin/settings/clinics'
-export const BIOFLYT_THERAPISTS_PATH = '/bioflow/admin/settings/therapists'
-export const BIOFLYT_STUDIES_PATH = '/bioflow/admin/settings/studies'
-export const BIOFLYT_STUDY_CREATE_PATH = '/bioflow/admin/settings/study/create'
-export const BIOFLYT_NOTIFICATIONS_PATH =
+export const BIOFLYT_ADMIN_GLOBAL_ACTIVITIES_PATH = '/bioflow/admin/activities'
+export const BIOFLYT_ADMIN_CLINICS_PATH = '/bioflow/admin/settings/clinics'
+export const BIOFLYT_ADMIN_THERAPISTS_PATH =
+  '/bioflow/admin/settings/therapists'
+export const BIOFLYT_ADMIN_STUDIES_PATH = '/bioflow/admin/settings/studies'
+export const BIOFLYT_ADMIN_STUDY_CREATE_PATH =
+  '/bioflow/admin/settings/study/create'
+export const BIOFLYT_ADMIN_NOTIFICATIONS_PATH =
   '/bioflow/admin/settings/notifications'
+export const BIOFLYT_GROUPS_PATH = '/bioflow/groups'
+export const BIOFLYT_GROUP_PATH = '/bioflow/groups/:id'
+export const BIOFLYT_GROUP_EDIT_PATH = '/bioflow/groups/:id/edit'
+export const BIOFLYT_GROUP_CREATE_PATH = '/bioflow/group/create'
+export const BIOFLYT_GROUP_PATIENTS_PATH = '/bioflow/groups/:id/patients'
+export const BIOFLYT_PATIENTS_PATH = '/bioflow/patients'
+export const BIOFLYT_CHAT_PATH = '/bioflow/chat'
+export const BIOFLYT_CHAT_GROUP_PATH = '/bioflow/groups/:id/group-chat'
+export const BIOFLYT_CHAT_ADMIN_PATH = '/bioflow/groups/:id/admin-chat'
+export const BIOFLYT_ACTIVITIES_PATH = '/bioflow/groups/:id/activities'
+export const BIOFLYT_NOTIFICATIONS_PATH = '/bioflow/notifications'
 
 export default {
-  BIOFLYT_PATH,
+  BIOFLYT_ADMIN_PATH,
+  BIOFLYT_ADMIN_GROUPS_PATH,
+  BIOFLYT_ADMIN_GROUP_SHOW_PATH,
+  BIOFLYT_ADMIN_GROUP_EDIT_PATH,
+  BIOFLYT_ADMIN_GROUP_CREATE_PATH,
+  BIOFLYT_ADMIN_GROUP_ACTIVITIES_PATH,
+  BIOFLYT_ADMIN_GROUP_PATIENTS_PATH,
+  BIOFLYT_ADMIN_GLOBAL_ACTIVITIES_PATH,
+  BIOFLYT_ADMIN_CLINICS_PATH,
+  BIOFLYT_ADMIN_THERAPISTS_PATH,
+  BIOFLYT_ADMIN_STUDIES_PATH,
+  BIOFLYT_ADMIN_STUDY_CREATE_PATH,
+  BIOFLYT_ADMIN_NOTIFICATIONS_PATH,
+  BIOFLYT_GROUP_PATH,
   BIOFLYT_GROUPS_PATH,
-  BIOFLYT_GROUP_SHOW_PATH,
+  BIOFLYT_PATIENTS_PATH,
   BIOFLYT_GROUP_EDIT_PATH,
   BIOFLYT_GROUP_CREATE_PATH,
-  BIOFLYT_GROUP_ACTIVITIES_PATH,
   BIOFLYT_GROUP_PATIENTS_PATH,
-  BIOFLYT_GLOBAL_ACTIVITIES_PATH,
-  BIOFLYT_CLINICS_PATH,
-  BIOFLYT_THERAPISTS_PATH,
-  BIOFLYT_STUDIES_PATH,
-  BIOFLYT_STUDY_CREATE_PATH,
+  BIOFLYT_CHAT_PATH,
+  BIOFLYT_CHAT_GROUP_PATH,
+  BIOFLYT_CHAT_ADMIN_PATH,
+  BIOFLYT_ACTIVITIES_PATH,
   BIOFLYT_NOTIFICATIONS_PATH
 }
