6b79725efb6b77befd96b12439bf27749d5b61e4
sashka2131 <uhucbeucg@gmail.com>
2021-10-12 16:56:43 +0300

    add all routes constants

---
+++ src/app/constants/paths.js
@@ -0,0 +1,33 @@
+export const BIOFLYT_PATH = '/bioflow/admin'
+
+export const BIOFLYT_GROUPS_PATH = '/bioflow/admin/groups'
+export const BIOFLYT_GROUP_SHOW_PATH = '/bioflow/admin/groups/:id'
+export const BIOFLYT_GROUP_EDIT_PATH = '/bioflow/admin/groups/:id/edit'
+export const BIOFLYT_GROUP_CREATE_PATH = '/bioflow/admin/group/create'
+export const BIOFLYT_GROUP_ACTIVITIES_PATH =
+  '/bioflow/admin/groups/:id/activities'
+export const BIOFLYT_GROUP_PATIENTS_PATH = '/bioflow/admin/groups/:id/patients'
+
+export const BIOFLYT_GLOBAL_ACTIVITIES_PATH = '/bioflow/admin/activities'
+export const BIOFLYT_CLINICS_PATH = '/bioflow/admin/settings/clinics'
+export const BIOFLYT_THERAPISTS_PATH = '/bioflow/admin/settings/therapists'
+export const BIOFLYT_STUDIES_PATH = '/bioflow/admin/settings/studies'
+export const BIOFLYT_STUDY_CREATE_PATH = '/bioflow/admin/settings/study/create'
+export const BIOFLYT_NOTIFICATIONS_PATH =
+  '/bioflow/admin/settings/notifications'
+
+export default {
+  BIOFLYT_PATH,
+  BIOFLYT_GROUPS_PATH,
+  BIOFLYT_GROUP_SHOW_PATH,
+  BIOFLYT_GROUP_EDIT_PATH,
+  BIOFLYT_GROUP_CREATE_PATH,
+  BIOFLYT_GROUP_ACTIVITIES_PATH,
+  BIOFLYT_GROUP_PATIENTS_PATH,
+  BIOFLYT_GLOBAL_ACTIVITIES_PATH,
+  BIOFLYT_CLINICS_PATH,
+  BIOFLYT_THERAPISTS_PATH,
+  BIOFLYT_STUDIES_PATH,
+  BIOFLYT_STUDY_CREATE_PATH,
+  BIOFLYT_NOTIFICATIONS_PATH
+}
