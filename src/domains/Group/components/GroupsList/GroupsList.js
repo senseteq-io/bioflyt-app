4244fc05b2023c3d5ec32f01de8add910907d8f4
Maxim Makarov <maxim.makarov14@gmail.com>
2021-10-18 15:32:55 +0300

    Add filtering for groups all list

--- src/domains/Group/components/GroupsList/GroupsList.js
+++ src/domains/Group/components/GroupsList/GroupsList.js
@@ -1,8 +1,15 @@
+import { Box, Title } from '@qonsoll/react-design'
+import { List } from 'antd'
+
+import { useClinicContext } from 'app/domains/Clinic/contexts'
 import { GroupAdvancedView } from 'bioflow/domains/Group/components'
-import React from 'react'
+import firebase from 'firebase'
+import _ from 'lodash'
+import React, { useEffect, useState } from 'react'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { useHistory } from 'react-router-dom'
 import { useTranslations } from '@qonsoll/translation'
-import { ListWithCreate } from 'app/components'
+import { AddItem } from 'app/components'
 import { GROUPS } from 'bioflow/constants/collections'
 import { useBioflowAccess } from 'bioflow/hooks'
 import {
@@ -70,6 +77,17 @@ function GroupsList() {
   const { t } = useTranslations()
   const history = useHistory()
   const { isAdmin } = useBioflowAccess()
+  const { _id: clinicId } = useClinicContext()
+  // const [{ height }, boxRef] = useSize()
+
+  // [DATA FETCH]
+  const [list] = useCollectionData(
+    clinicId &&
+      firebase.firestore().collection(GROUPS).where('clinicId', '==', clinicId)
+  )
+
+  // [COMPONENT_STATE_HOOKS]
+  const [sortedList, setSortedList] = useState({})
 
   // [CLEAN_FUNCTIONS]
   const goToCreateGroup = () =>
@@ -77,22 +95,69 @@ function GroupsList() {
       isAdmin ? BIOFLOW_ADMIN_GROUP_CREATE_PATH : BIOFLOW_GROUP_CREATE_PATH
     )
 
+  // [USE_EFFECTS]
+  useEffect(() => {
+    if (list) {
+      const statuses = _.uniq(MOCK_GROUPS.map(({ status }) => status))
+      const filteredList = {}
+
+      statuses.forEach((status) => {
+        filteredList[status] = _.filter(
+          MOCK_GROUPS,
+          (item) => item.status === status
+        )
+      })
+      setSortedList(filteredList)
+    }
+  }, [list])
   return (
-    <ListWithCreate
+    <>
+      <Box mb={4}>
+        <AddItem
+          onClick={goToCreateGroup}
+          variant="large"
+          createText={t('Add group')}
+        />
+      </Box>
+
+      {sortedList['DRAFT'] && (
+        <GroupFilteredList status={t('Draft')} data={sortedList['DRAFT']} />
+      )}
+      {sortedList['ONGOING'] && (
+        <GroupFilteredList status={t('Ongoing')} data={sortedList['ONGOING']} />
+      )}
+      {sortedList['FUTURE'] && (
+        <GroupFilteredList status={t('Future')} data={sortedList['FUTURE']} />
+      )}
+      {sortedList['FINISHED'] && (
+        <GroupFilteredList
+          status={t('Finished')}
+          data={sortedList['FINISHED']}
+        />
+      )}
+    </>
+  )
+}
+
+const GroupFilteredList = ({ status, data }) => (
+  <Box mb={3}>
+    <Title level={4} mb={2}>
+      {status}
+    </Title>
+    <List
       grid={{
         gutter: [32, 16],
         column: 1
       }}
-      createVariant="large"
-      createText={t('Add group')}
-      onCreate={goToCreateGroup}
-      collection={GROUPS}
-      withCreate
-      dataSource={MOCK_GROUPS}>
-      <GroupAdvancedView />
-    </ListWithCreate>
-  )
-}
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item key={item._id} style={{ width: '100%' }}>
+          <GroupAdvancedView {...item} />
+        </List.Item>
+      )}
+    />
+  </Box>
+)
 
 GroupsList.propTypes = {}
 
